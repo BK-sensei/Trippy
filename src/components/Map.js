c7e0f935177b3a24113b687ad954b9c4e2fb3e61
Nascimento95 <marco-pereira@hotmail.fr>
2021-11-24 11:10:40 +0100

    le maps va direct a la localisation de la ville terminer

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,65 +1,38 @@
-import { useEffect, useState } from "react"
-import { useParams } from 'react-router-dom'
 import GoogleMapReact from 'google-map-react'
 import styled from 'styled-components'
 import Marker from "./Marker"
+// import Hotels from "../pages/Hotels"
+
 const MapContainer = styled.div`
   height: 100vh;
-  width: 69%;
+  width: 50%;
 `
-
-
-const Map = () => {
-    const [ hotels , setHotels ] = useState(null)
-    // const [ location , setLocation ] = useState(null)
-    const [myPosition, setMyPosition] = useState(null)
-    const { city } = useParams()
-    useEffect(() => {
-        fetch( `https://trippy-konexio.herokuapp.com/api/hotels/city/${city}`)
-            .then(reponse => reponse.json())
-            .then(result => setHotels(result))
-        // console.log("Récupération de la localisation...")
-    
-        navigator.geolocation.getCurrentPosition(
-          location => {
-            console.log(location.coords)
-            setMyPosition({
-              lat: location.coords.latitude,
-              lng: location.coords.longitude
-            })
-          },
-          error => {
-            console.log(error)
-          }
-        )
-      }, [])
+const Map = (props) => {    
+//   console.log("props hotel",props);
+  const {center} = props.hotels
     
     return (
         <>
-            {hotels === null ? 
-            (<p>en cour de chargement ...</p>) 
-            :
-            ( 
-                <MapContainer>
-                    <GoogleMapReact
-                    bootstrapURLKeys={{ key: "" }}
-                    defaultCenter={myPosition}
-                    defaultZoom={14}
-                    >
-                        {hotels.results.map((hotel,index) => 
+            <MapContainer>
+                <GoogleMapReact
+                bootstrapURLKeys={{ key: "" }}
+                defaultCenter={{
+                    lat: center.lat,
+                    lng: center.lon
+                }}
+                defaultZoom={14}
+                >
+                    {props.hotels.results.map((hotel,index) => 
                         <Marker 
                         key={hotel.name}
                         lat={hotel.location.lat}
                         lng={hotel.location.lon}
                         prix={hotel.price}
                         />
-                        
-                    ) }
-                    
-                    </GoogleMapReact>
-                </MapContainer>
-            ) }
-            
+                    ) } 
+                
+                </GoogleMapReact>
+            </MapContainer>
         </>  
     )
   }
--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,9 +1,9 @@
 import { useState, useEffect, } from "react";
 import { useParams } from 'react-router-dom'
 import Map from "../components/Map";
-
 // import { Link } from "react-router-dom";
 import styled from 'styled-components'
+
 const Container = styled.div` 
 border : 1px solid black;
 margin-top :10px;
@@ -20,20 +20,17 @@ img {
 }
 `;
 const Span = styled.span` 
-font-weight: bold;
+    font-weight: bold;
 `;
 const Container1 = styled.div` 
-margin : 0px 0px 0px 10px;
-height: 100vh;
-width : 500px;
-overflow:scroll;
-
+    margin : 0px 0px 0px 10px;
+    height: 100vh;
+    width : 500px;
+    overflow:scroll;
 `;
 const Container2 = styled.div` 
 display:flex;
 `;
-
-
 
 const Hotels = () => {
     const [ hotels , setHotels ] = useState(null)
@@ -46,7 +43,7 @@ const Hotels = () => {
 
     }, []);
 
-    console.log(hotels);
+    // console.log(hotels);
     return (
       <>
         { hotels === null ? 
@@ -56,17 +53,16 @@ const Hotels = () => {
                     <Container1>
                         <h1>la liste d'hotel à {city} </h1>
                         {hotels.results.map((hotel, index) => 
-                        <Container key={`${hotel.name}${index}`} >
-                            <img src={`https://trippy-konexio.herokuapp.com${hotel.pictures[29]}`} />
-                            <h1>{hotel.name} </h1>
-                            <p> Adresse : {hotel.address}</p>
-                            <p> Le prix :<Span> {hotel.price}€</Span></p>
-                            
-                        </Container>
-                        
+                            <Container key={`${hotel.name}${index}`} >
+                                <img src={`https://trippy-konexio.herokuapp.com${hotel.pictures[29]}`} />
+                                <h1>{hotel.name} </h1>
+                                <p> Adresse : {hotel.address}</p>
+                                <p> Le prix :<Span> {hotel.price}€</Span></p>
+                                
+                            </Container>
                         )}   
                     </Container1>
-                    <Map /> 
+                    <Map hotels = {hotels} /> 
                 </Container2>   
             )    
         }
