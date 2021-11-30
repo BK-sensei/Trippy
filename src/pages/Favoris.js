b01ccc3f3f454359f14fe33767395c9c568c5f1d
Nascimento95 <marco-pereira@hotmail.fr>
2021-11-30 10:43:28 +0100

    le fichier le plus a jour style favorit

--- src/pages/Favoris.js
+++ src/pages/Favoris.js
@@ -3,44 +3,99 @@ import styled from 'styled-components'
 import Nav from '../components/Nav'
 import {BsFillTrashFill} from 'react-icons/bs'
 
+const FavoritePage = styled.div`
+    background: #EEE6DD;
+`
+const FavTitle = styled.h1`
+    text-align: center;
+    font-family: 'Poppins', sans-serif;
+    border-top: thick double;
+    border-bottom: thick double;
+    padding: 5px;
+    margin-top: 0;
+
+    @media (max-width: 400px){
+        font-size: 30px;
+    }
+`
 const ContainerFav = styled.div`
-    // border : 1px solid black;
     width : 100%;
     display: flex;
     @media(max-width : 900px){
         flex-wrap : wrap;
-    }
-    
+    } 
 `
 const ContainerNav = styled.div`
     background : #EB8D61;
 `
 const Container = styled.div`
-    display: flex;
-    @media(max-width : 900px){
+    padding: 20px;
+
+    @media(max-width : 850px){
         flex-direction : column;
+    }
+`
+const FavoriteList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: auto;
+    font-family: 'Poppins', sans-serif;
+
+    @media(max-width : 1200px){
+        grid-template-columns: repeat(2, 1fr)
+    }
+    @media(max-width : 850px){
+        display: flex;
+        flex-direction: column;
     }
 `
 const ContainerCardFav = styled.div`
     border : 1px solid black;
-    border-radius : 20px;
+    border-radius : 10px;
     width : 100%;
-    margin : 20px 0px 20px 20px ; 
-
-p{
+    margin : 10px 10px; 
+    background-color: #201F1F;
+    color: #E5E5E5;
     padding: 10px;
-}
-    
+    h2, p{
+        padding: 10px;
+        margin: 0;
+    }  
+
+    @media (max-width: 400px){
+        h2{
+            font-size: 20px;
+        }
+        p{
+            font-size: 12px;
+        }
+    }
+`
+const Price = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #E5E5E5;
+    color: #201F1F;
+    margin-left : 10px;
+    border-radius: 5px;
+    padding: 5px 5px;
+
+    @media (max-width: 400px){
+        font-size: 12px;
+    }
 `
 const Icon = styled.div`
     display:flex;
     justify-content : space-between;
     align-items:center;
-    
+    padding-right: 20px;
+    padding-bottom: 10px;
 `
 const Span = styled.span`
     font-weight: bold;
 `
+
 const Favoris = () =>{
     
     const [dataFavoris, setDataFavoris] = useState([])
@@ -67,35 +122,37 @@ const Favoris = () =>{
     }
     
     return (
-        <>
+        <FavoritePage>
             <ContainerNav>
                     <Nav />
             </ContainerNav>
             <Container>
-                { dataFavoris === [] ? 
-                (<p>pas de favoris</p>) 
-                :
-                (
-            
-                    dataFavoris.map( (favoris,index) => 
-                        <ContainerFav key={index} >
-                            <ContainerCardFav>
-                                <h1>{favoris.result.name} </h1>
-                                <p> <Span>address :</Span> {favoris.result.address}</p>
-                                <p> <Span> phone number :</Span> {favoris.result.phone} </p>
-                                <Icon>
-                                    <p> <Span> price : </Span> {favoris.result.price}€</p>
-                                    <BsFillTrashFill onClick={()=> DeleteFav (index)} />
-                                </Icon>
-                            </ContainerCardFav> 
-                        </ContainerFav>
-                    )
-                    
-                    
-                )}
-            
+                <FavTitle>My Favorite Hotels</FavTitle>
+                <FavoriteList>
+                    { dataFavoris === [] ? 
+                    (<p>pas de favoris</p>) 
+                    :
+                    (
+                
+                        dataFavoris.map( (favoris,index) => 
+                            <ContainerFav key={index} >
+                                <ContainerCardFav>
+                                    <h2>{favoris.result.name} </h2>
+                                    <p> <Span>Address :</Span> {favoris.result.address}</p>
+                                    <p> <Span> Phone number :</Span> {favoris.result.phone} </p>
+                                    <Icon>
+                                        <Price><Span> Price : {favoris.result.price}€</Span></Price>
+                                        <BsFillTrashFill onClick={()=> DeleteFav (index)} />
+                                    </Icon>
+                                </ContainerCardFav> 
+                            </ContainerFav>
+                        )
+                        
+                        
+                    )}
+                </FavoriteList>
             </Container>
-        </>    
+        </FavoritePage>    
     )
 }
 export default Favoris
